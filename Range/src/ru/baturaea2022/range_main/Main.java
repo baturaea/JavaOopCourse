2c7d4239235d96364ceef73f0160a10f346fbfe0
batura <jonb@narod.ru>
2022-01-29 15:51:33 +0700

    Часть 1. Создать класс Range (непрерывный вещественный числовой диапазон на прямой). В нём: 1.	Объявить два вещественных поля from, to 2.	Описать конструктор, при помощи которого заполняются поля 3.	Реализовать геттеры и сеттеры для полей 4.	Сделать метод для получения длины диапазона 5.	Сделать метод isInside, который принимает вещественное число и возвращает boolean – результат проверки, принадлежит ли число диапазону

--- Range/src/ru/baturaea2022/range_main/Main.java
+++ Range/src/ru/baturaea2022/range_main/Main.java
@@ -1,7 +1,33 @@
 package ru.baturaea2022.range_main;
 
+import ru.baturaea2022.range.Range;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Укажите начало диапазона: ");
+        double fromNumber = scanner.nextDouble();
+
+        System.out.print("Укажите конец диапазона: ");
+        double toNumber = scanner.nextDouble();
+
+        System.out.print("Укажите число для проверки вхождения в диапазон: ");
+        double checkNumber = scanner.nextDouble();
+
+        Range range = new Range(fromNumber, toNumber);
+        System.out.printf("Длинна диапазона от %.2f до %.2f равна %.2f%n", range.getFrom(), range.getTo(), range.getLength());
+        String checkingResult = range.isInside(checkNumber) ? "входит в указанный диапазон" : "не входит в указанный диапазон";
+        System.out.printf("Число %.2f %s%n", checkNumber, checkingResult);
+
+        System.out.print("Измените начало диапазона: ");
+        range.setFrom(scanner.nextDouble());
+
+        System.out.print("Измените конец диапазона: ");
+        range.setTo(scanner.nextDouble());
+
+        System.out.printf("Длинна нового диапазона от %.2f до %.2f равна %.2f%n", range.getFrom(), range.getTo(), range.getLength());
+        checkingResult = range.isInside(checkNumber) ? "входит в новый диапазон" : "не входит в новый диапазон";
+        System.out.printf("Число %.2f %s%n", checkNumber, checkingResult);    }
 }
