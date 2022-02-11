f5651ec47c3bb362279d5a59747b89d8fd757911
batura <jonb@narod.ru>
2022-02-11 20:17:55 +0700

    Задача Range часть2 Добавлены методы getIntersection, getUnion, getDifference

--- Range/src/ru/baturaea2022/range/Range.java
+++ Range/src/ru/baturaea2022/range/Range.java
@@ -30,6 +30,40 @@ public class Range {
     }
 
     public boolean isInside(double checkNumber) {
-        return checkNumber >= from && checkNumber <= to;
+        return checkNumber > from && checkNumber < to;
+    }
+
+    public Range getIntersection(Range range) {
+        if (this.getFrom() <= range.getFrom() && this.getTo() >= range.getFrom()
+                || this.getTo() >= range.getTo() && this.getTo() <= range.getTo()) {
+            return new Range(Math.max(range.getFrom(), this.getFrom()), Math.min(range.getTo(), this.getTo()));
+        }
+
+        return null;
+    }
+
+    public Range[] getUnion(Range range) {
+        if (this.isInside(range.getFrom()) || this.isInside(range.getTo())) {
+            return new Range[]{new Range(Math.min(this.getFrom(), range.getFrom()), Math.max(this.getTo(), range.getTo()))};
+        }
+
+        return new Range[]{this, range};
+    }
+
+    public Range[] getDifference(Range range) {
+        if (this.getFrom() == range.getFrom() && this.getTo() == range.getTo()) {
+            return new Range[]{new Range(0, 0)};
+        } else if (this.isInside(range.getFrom()) && this.isInside(range.getTo())) {
+            return new Range[]{
+                    new Range(this.getFrom(), range.getFrom()),
+                    new Range(range.getTo(), this.getTo())
+            };
+        } else if (this.isInside(range.getFrom())) {
+            return new Range[]{new Range(this.getFrom(), range.getFrom())};
+        } else if (this.isInside(range.getTo())) {
+            return new Range[]{new Range(range.getTo(), this.getTo())};
+        }
+
+        return new Range[]{this};
     }
 }
--- Range/src/ru/baturaea2022/range_main/Main.java
+++ Range/src/ru/baturaea2022/range_main/Main.java
@@ -17,19 +17,43 @@ public class Main {
         System.out.print("Укажите число для проверки вхождения в диапазон: ");
         double checkNumber = scanner.nextDouble();
 
-        Range range = new Range(fromNumber, toNumber);
-        System.out.printf("Длинна диапазона от %.2f до %.2f равна %.2f%n", range.getFrom(), range.getTo(), range.getLength());
-        String checkingResult = range.isInside(checkNumber) ? "входит в указанный диапазон" : "не входит в указанный диапазон";
+        Range range1 = new Range(fromNumber, toNumber);
+        System.out.printf("Длинна диапазона от %.2f до %.2f равна %.2f%n", range1.getFrom(), range1.getTo(), range1.getLength());
+        String checkingResult = range1.isInside(checkNumber) ? "входит в указанный диапазон" : "не входит в указанный диапазон";
         System.out.printf("Число %.2f %s%n", checkNumber, checkingResult);
 
         System.out.print("Измените начало диапазона: ");
-        range.setFrom(scanner.nextDouble());
+        range1.setFrom(scanner.nextDouble());
 
         System.out.print("Измените конец диапазона: ");
-        range.setTo(scanner.nextDouble());
+        range1.setTo(scanner.nextDouble());
 
-        System.out.printf("Длинна нового диапазона от %.2f до %.2f равна %.2f%n", range.getFrom(), range.getTo(), range.getLength());
-        checkingResult = range.isInside(checkNumber) ? "входит в новый диапазон" : "не входит в новый диапазон";
+        System.out.printf("Длинна нового диапазона от %.2f до %.2f равна %.2f%n", range1.getFrom(), range1.getTo(), range1.getLength());
+        checkingResult = range1.isInside(checkNumber) ? "входит в новый диапазон" : "не входит в новый диапазон";
         System.out.printf("Число %.2f %s%n", checkNumber, checkingResult);
+
+        Range range2 = new Range(1, 2);
+        Range range3 = new Range(4, 6);
+
+        System.out.printf("range2 - {%f, %f}, range3 - {%f, %f}%n", range2.getFrom(), range2.getTo(), range3.getFrom(), range3.getTo());
+
+        Range range4 = range2.getIntersection(range3);
+        if (range4 != null) {
+            System.out.printf("range2.getIntersection(range3) = {%f, %f}%n", range4.getFrom(), range4.getTo());
+        } else {
+            System.out.println("range2 и range3 не пересекаются => range4 = null");
+        }
+
+        System.out.println("range2.getUnion(range3)");
+        Range[] arrayRanges1 = range2.getUnion(range3);
+        for (Range r : arrayRanges1) {
+            System.out.printf("range - {%f, %f}%n", r.getFrom(), r.getTo());
+        }
+
+        System.out.println("range2.getDifference(range3)");
+        Range[] arrayRanges2 = range2.getDifference(range3);
+        for (Range r : arrayRanges2) {
+            System.out.printf("range - {%f, %f}%n", r.getFrom(), r.getTo());
+        }
     }
 }
