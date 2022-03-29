a6aed8ae0f636c2118fe48a93b2452dfe85243d8
batura <jonb@narod.ru>
2022-03-29 23:53:51 +0700

    Исправление по задаче ArrayListHome

--- ArrayListHome/src/ru/baturaea2022/arraylisthome/Main.java
+++ ArrayListHome/src/ru/baturaea2022/arraylisthome/Main.java
@@ -3,47 +3,52 @@ package ru.baturaea2022.arraylisthome;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
-        ArrayList<Integer> list1 = new ArrayList<>();
+
+    public static void main(String[] args) {
+        //Прочитать в список все строки из файла
+        ArrayList<String> listStrings = new ArrayList<>();
 
         try (Scanner scanner = new Scanner(new FileInputStream("ArrayListHome/src/ru/baturaea2022/arraylisthome/input.txt"))) {
             while (scanner.hasNextLine()) {
-                list1.add(scanner.nextInt());
+                listStrings.add(scanner.nextLine());
             }
+
+            System.out.println("Прочитанный из файла список: ");
+            System.out.println(listStrings);
+        } catch (FileNotFoundException e) {
+            System.out.println("File 'input.txt' not found.");
         }
 
-        System.out.println(list1);
+        //Есть список из целых чисел. Удалить из него все четные числа.
+        ArrayList<Integer> listIntegers = new ArrayList<>(Arrays.asList(2, 1, 7, 7, 3, 4, 3, 5, 6, 6, 7, 8, 7));
+        System.out.println("Исходный список чисел: ");
+        System.out.println(listIntegers);
 
-        for (int i = 0; i < list1.size(); i++) {
-            if (list1.get(i) % 2 == 0) {
-                list1.remove(i);
+        for (int i = 0; i < listIntegers.size(); i++) {
+            if (listIntegers.get(i) % 2 == 0) {
+                listIntegers.remove(i);
                 i--;
             }
         }
 
-        System.out.println(list1);
-        ArrayList<Integer> list2 = new ArrayList<>();
+        System.out.println("Список без четных чисел: ");
+        System.out.println(listIntegers);
 
-        for (int i = 0; i < list1.size(); i++) {
-            Integer element = list1.get(i);
-            boolean isRepeat = false;
+        //Надо создать новый список, в котором будут элементы первого списка в таком же порядке, но без повторений
+        ArrayList<Integer> listUniquesElements = new ArrayList<>(listIntegers.size());
 
-            for (int j = 0; j < i; j++) {
-                if (element.equals(list1.get(j))) {
-                    isRepeat = true;
-                    break;
-                }
-            }
-
-            if (!isRepeat) {
-                list2.add(element);
+        for (Integer element : listIntegers) {
+            if (!listUniquesElements.contains(element)) {
+                listUniquesElements.add(element);
             }
         }
 
-        System.out.println(list2);
+        System.out.println("Cписок без дубликатов: ");
+        System.out.println(listUniquesElements);
     }
 }
 
