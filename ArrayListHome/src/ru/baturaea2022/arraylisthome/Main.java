443aa10597b2bf057fd6986ca71464f98c2732fd
batura <jonb@narod.ru>
2022-05-04 09:11:51 +0700

    Исправления по задаче ArrayListHome Исправления по задаче Lambda

--- ArrayListHome/src/ru/baturaea2022/arraylisthome/Main.java
+++ ArrayListHome/src/ru/baturaea2022/arraylisthome/Main.java
@@ -1,53 +1,53 @@
 package ru.baturaea2022.arraylisthome;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
-        //Прочитать в список все строки из файла
-        ArrayList<String> listStrings = new ArrayList<>();
+        // Прочитать в список все строки из файла
+        ArrayList<String> stringsList = new ArrayList<>();
+        String inputFile = "ArrayListHome/src/ru/baturaea2022/arraylisthome/input.txt";
 
-        try (Scanner scanner = new Scanner(new FileInputStream("ArrayListHome/src/ru/baturaea2022/arraylisthome/input.txt"))) {
-            while (scanner.hasNextLine()) {
-                listStrings.add(scanner.nextLine());
+        try (BufferedReader in =
+                     new BufferedReader(new FileReader(inputFile))) {
+            String line;
+            while ((line = in.readLine()) != null) {
+                stringsList.add(line);
             }
 
-            System.out.println("Прочитанный из файла список: ");
-            System.out.println(listStrings);
-        } catch (FileNotFoundException e) {
-            System.out.println("File 'input.txt' not found.");
+            System.out.println("Прочитанный из файла список:");
+            System.out.println(stringsList);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        // Есть список из целых чисел. Удалить из него все четные числа.
+        ArrayList<Integer> integerList = new ArrayList<>(Arrays.asList(2, 1, 7, 7, 3, 4, 3, 5, 6, 6, 7, 8, 7));
+        System.out.println("Исходный список чисел:");
+        System.out.println(integerList);
 
-        //Есть список из целых чисел. Удалить из него все четные числа.
-        ArrayList<Integer> listIntegers = new ArrayList<>(Arrays.asList(2, 1, 7, 7, 3, 4, 3, 5, 6, 6, 7, 8, 7));
-        System.out.println("Исходный список чисел: ");
-        System.out.println(listIntegers);
-
-        for (int i = 0; i < listIntegers.size(); i++) {
-            if (listIntegers.get(i) % 2 == 0) {
-                listIntegers.remove(i);
+        for (int i = 0; i < integerList.size(); i++) {
+            if (integerList.get(i) % 2 == 0) {
+                integerList.remove(i);
                 i--;
             }
         }
 
-        System.out.println("Список без четных чисел: ");
-        System.out.println(listIntegers);
+        System.out.println("Список без четных чисел:");
+        System.out.println(integerList);
 
-        //Надо создать новый список, в котором будут элементы первого списка в таком же порядке, но без повторений
-        ArrayList<Integer> listUniquesElements = new ArrayList<>(listIntegers.size());
+        // Надо создать новый список, в котором будут элементы первого списка в таком же порядке, но без повторений
+        ArrayList<Integer> uniqueElementsList = new ArrayList<>(integerList.size());
 
-        for (Integer element : listIntegers) {
-            if (!listUniquesElements.contains(element)) {
-                listUniquesElements.add(element);
+        for (Integer element : integerList) {
+            if (!uniqueElementsList.contains(element)) {
+                uniqueElementsList.add(element);
             }
         }
 
-        System.out.println("Список без дубликатов: ");
-        System.out.println(listUniquesElements);
+        System.out.println("Список без дубликатов:");
+        System.out.println(uniqueElementsList);
     }
 }
--- Lambda/src/ru/baturaea2022/lambda/Main.java
+++ Lambda/src/ru/baturaea2022/lambda/Main.java
@@ -3,9 +3,7 @@ package ru.baturaea2022.lambda;
 import java.util.*;
 import java.util.stream.Collectors;
 
-
 public class Main {
-
     public static void main(String[] args) {
         List<Person> personsList = Arrays.asList(
                 new Person("Иван", 21),
@@ -21,33 +19,31 @@ public class Main {
         );
 
         // А) получить список уникальных имен
-        List<String> uniqueNameList = personsList.stream()
+        String uniqueNameList = personsList.stream()
                 .map(Person::getName)
                 .distinct()
-                .peek(System.out::println)
-                .collect(Collectors.toList());
+                .collect(Collectors.joining(", ", "", "."));
 
         // Б) вывести список уникальных имен в формате: Имена: Иван, Сергей, Петр.
         System.out.println();
-        System.out.println("Уникальные имена: " + String.join(", ", uniqueNameList));
+        System.out.println("Уникальные имена: " + uniqueNameList);
 
         // В) получить список людей младше 18, посчитать для них средний возраст
         System.out.println();
         List<Person> youngPersonsList = personsList.stream()
                 .filter(p -> p.getAge() < 18)
-                .peek(person -> System.out.println("Name: " + person.getName() + " Age: " + person.getAge()))
                 .collect(Collectors.toList());
-
-        System.out.printf("Средний возраст людей моложе 18 = %.0f %n", youngPersonsList.stream()
+        System.out.println("Список людей младше 18: " + youngPersonsList);
+        double youngPersonsLis = youngPersonsList.stream()
                 .mapToDouble(Person::getAge)
-                .average().orElse(0.0));
+                .average()
+                .orElse(0.0);
+        System.out.printf("Средний возраст людей моложе 18 = %.0f %n", youngPersonsLis);
 
         // Г) при помощи группировки получить Map, в котором ключи – имена, а значения – средний возраст
         System.out.println();
         Map<String, Double> averageAgeByName = personsList.stream()
-                .collect(Collectors.groupingBy(Person::getName,
-                        Collectors.averagingInt(Person::getAge)));
-
+                .collect(Collectors.groupingBy(Person::getName, Collectors.averagingInt(Person::getAge)));
         averageAgeByName.forEach((name, age) ->
                 System.out.printf("У людей с именем - %s , средний возраст = %.0f%n", name, age));
 
@@ -57,7 +53,7 @@ public class Main {
                 .sorted(Comparator.comparing(Person::getAge).reversed())
                 .filter(p -> p.getAge() > 19)
                 .filter(p -> p.getAge() < 45)
-                .peek(person -> System.out.println("Name: " + person.getName() + " Age: " + person.getAge()))
                 .collect(Collectors.toList());
+        System.out.println("Список людей, возраст которых от 20 до 45: " + rangeAgePersonsList);
     }
 }
